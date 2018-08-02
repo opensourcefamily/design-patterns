2ee5041323566e20b9df57800a3e16e61a581896
leslie <panxiang_work@163.com>
2018-08-02 11:27:30 +0800

    单例模式

---
+++ multition-pattern/src/main/java/com/studyinghome/multition/MultitionPatternApplication.java
@@ -0,0 +1,12 @@
+package com.studyinghome.multition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitionPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitionPatternApplication.class, args);
+    }
+}
---
+++ multition-pattern/src/test/java/com/studyinghome/multition/MultitionPatternApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studyinghome.multition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultitionPatternApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
